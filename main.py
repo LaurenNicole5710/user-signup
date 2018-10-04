084be7a6dc448e33e74e4859e4c1777e41ab3b94
Lauren Nicole <theofferingstone@gmail.com>
2018-10-03 22:19:28 -0500

    Deleted remember me button

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ app.config['DEBUG'] = True
 def index():
     return render_template('index.html')
 
-@app.route("/", methods=['POST'])
-def validate_data():
+@app.route("/validate", methods=['POST'])
+def validate_form():
     username = request.form['username']
     password = request.form['password']
     confirm_password = request.form['confirm_password']
-    email = request.form['email']
+    user_email = request.form['user_email']
 
     username_error = ''
     password_error = ''
@@ -28,13 +28,21 @@ def validate_data():
     elif ' ' in username:
         username_error = "Username may not contain any spaces."
         username = ''
+    elif len(username) <= 3 or len(username)>= 20: 
+        username_error = "Please enter a username between 3 and 20 characters."
+        username = ''
+        
     #password
     if password == '':
         passord_error = "Please enter a password."
+    elif len(password) <= 3 or len(password)>= 20:
+        password_error = "Please enter a password between 3 and 20 characters"
 
     #confirm
     if confirm_password == '':
         confirm_error = "Please confirm your password."
+    elif password != confirm_password:
+        confirm_error = "Passwords do not match."
     #email
     if user_email != '':
         if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", user_email):
@@ -42,23 +50,16 @@ def validate_data():
             user_email = ''
 
     #-- VERIFY LENGTH OF USERNAME AND PASSWORD <3  AND >20 --
-    if len(username) >= 3 or len(username)>= 20: 
-        username_error = "Please enter a username between 3 and 20 characters."
-        username = ''
-    if len(password) >= 3 or len(password)>= 20:
-        password_error = "Please enter a password between 3 and 20 characters"
-
+    
     #-- VERIFY PASSWORD AND PASSWORD CONFIRMATION MATCH --
-    if password != confirm_password:
-        confirm_error = "Passwords do not match."
     
     #-- IF NO ERRORS, RENDER WELCOME PAGE --
     if not username_error and not password_error and not confirm_error and not email_error:
         return render_template('welcome.html', username=username)
     else:
-        render_template('index.html', username=username, 
+        return render_template('index.html', username=username, 
                         password=password, 
-                        email=email, username_error=username_error,
+                        user_email=user_email, username_error=username_error,
                         password_error=password_error, email_error=email_error 
                         )
  
